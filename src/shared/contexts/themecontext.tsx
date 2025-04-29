2c0f5898ef97653862a09ec739e8a2095ceabdaa
leandrodias95 <leandrogit95@gmail.com>
2025-04-29 20:02:15 -0300

    adicionado tema escuro e troca de tema

---
+++ src/shared/contexts/themecontext.tsx
@@ -0,0 +1,37 @@
+import { createContext, ReactNode, useCallback, useContext, useMemo, useState } from 'react';
+import { DarkTheme, LightTheme } from '../themes';
+import { ThemeProvider } from '@emotion/react';
+import { Box } from '@mui/material';
+
+interface IThemeContextData{
+    themeName: 'light' | 'dark';
+    toggleTheme: () => void;  
+}
+interface IAppThemeProviderProps {
+    children: ReactNode;
+  }
+const ThemeContext =  createContext({} as IThemeContextData); 
+
+export const useAppThemeContext = () =>{
+    return useContext(ThemeContext); 
+}
+
+export const AppThemeProvider: React.FC<IAppThemeProviderProps> = ({children}) =>{
+    const [themeName, setThemeName] = useState<'light' | 'dark'>('light');
+    const toggleTheme = useCallback(()=>{
+        setThemeName(oldThemeName => oldThemeName ==='light'?'dark':'light' )
+    }, []);
+    const theme = useMemo(()=>{
+        if(themeName === 'light') return LightTheme; 
+        return DarkTheme; 
+    }, [themeName])
+    return(
+    <ThemeContext.Provider value={{themeName, toggleTheme}}>
+        <ThemeProvider theme ={DarkTheme}>
+        <Box width="100vw" height="100vh" bgcolor={theme.palette.background.default}>
+        {children}
+        </Box>
+        </ThemeProvider>
+    </ThemeContext.Provider>
+    );
+}
