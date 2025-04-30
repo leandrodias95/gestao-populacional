6bf58a2453254c95fe4fb5cf67863d42ddffbb9f
leandrodias95 <leandrogit95@gmail.com>
2025-04-30 08:24:17 -0300

    implementação do toggleDrawerOpen

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
 import { AppThemeProvider } from "./shared/contexts/ThemeContext";
 import { MenuLateral } from "./shared/components";
+import { DrawerProvider } from "./shared/contexts";
 
 function App() {
 	return (
 		<AppThemeProvider>
+		<DrawerProvider>
 			<BrowserRouter>
 				<MenuLateral>
 					<AppRoutes />
 				</MenuLateral>
 			</BrowserRouter>
+		</DrawerProvider>
 		</AppThemeProvider>
 	);
 }
--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,13 +1,16 @@
-import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from "@mui/material"
+import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from "@mui/material"
+import { useDrawerContext } from "../../contexts";
 
 interface IAppProviderProps {
     children: React.ReactNode
 }
 export const MenuLateral: React.FC<IAppProviderProps> = ({ children }) => {
     const theme =  useTheme(); 
+    const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+    const {isDrawerOpen, toggleDrawerOpen} =  useDrawerContext();
     return (
         <>
-            <Drawer variant='permanent'>
+            <Drawer open={isDrawerOpen}  onClose={toggleDrawerOpen}  variant={smDown ? 'temporary':'permanent'}>
                 <Box  width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
                 <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
                 <Avatar sx={{height: theme.spacing(12), width: theme.spacing(12) }} src="https://blogdoenem.com.br/wp-content/uploads/2016/03/1-30.gif" />
@@ -25,7 +28,7 @@ export const MenuLateral: React.FC<IAppProviderProps> = ({ children }) => {
                 </Box>
                 </Box>
             </Drawer>
-            <Box height="100vh" marginLeft={theme.spacing(28)}>
+            <Box height="100vh" marginLeft={smDown ? 0 : theme.spacing(28)}>
             {children}
             </Box>
         </>
